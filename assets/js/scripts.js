8095538192c543f7091e1df4b2e5a3822c2cb33d
Oguz Bilgin <oguz.bilginnn@gmail.com>
2019-12-20 11:03:36 +0300

    second commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-var listenerPlus, listenerNegative, orListener, xListener;
+var listenerPlus, listenerNegative, orListener,dataNumber, listenerClose;
 
 listenerPlus = document.getElementById('incButton');
 
@@ -12,44 +12,71 @@ orListen.addEventListener("click",copyBlock);
 
 listenerNegative.addEventListener("click",delFunction);
 
-xListener = document.getElementById('closeBlocks');
+listenerClose = document.getElementById('closeBlocks');
 
-xListener.addEventListener("click",deleteDiv);
-
-
+listenerClose.addEventListener('click',deleteDiv);
 
 
 
 
 
-function copySection(id){
+
+
+
+
+
+
+
+
+
+
+
+function copySection(){
 
    let sourceDiv = document.getElementById('sourceDiv');
    let clone = sourceDiv.cloneNode(true);
    let br = document.createElement('br');
    document.getElementById('clones').appendChild(br);
    let cloneDestination = document.getElementById('clones').appendChild(clone);
-   cloneDestination.setAttribute("id","cloned");
+   cloneDestination.setAttribute("id",'cloned');
    br.setAttribute("id","clonedbr");
 }
 
+/*function createUniqueId(id){
+   let counter = 1;
+   let uniqueId;
+   id = (typeof id ==="string")?id:"";
+   do{
+      uniqueId = id + counter++;
+   }while(document.getElementById(id)!==null);
+   return uniqueId;
+}*/
 
-function copyBlock(id){
+
+function copyBlock(){
+   let sourceBlock=document.getElementById('sourceBlock');
+   let mainBlock = document.getElementById('clonedBlock');
    let hr = document.createElement('HR');
-         document.getElementById('clonedBlock').appendChild(hr);
-    let sourceBlock = document.getElementById('sourceBlock');
-    let clone = sourceBlock.cloneNode(true);
-    let br = document.createElement('br');
-    let theclonedBlock = document.getElementById('clonedBlock').appendChild(clone);
-    document.getElementById('clonedBlock').appendChild(br);
-    hr.setAttribute('id','clonedHrInBlock');
-    br.setAttribute('id','clonedBrInBlock');
-    theclonedBlock.setAttribute("id","clonedDiv");
-    
-     
+   mainBlock.appendChild(hr);
+   let clone = sourceBlock.cloneNode(true);
+   let br = document.createElement('br');
+   let theclonedBlock = document.getElementById('clonedBlock').appendChild(clone);
+   theclonedBlock.setAttribute("id","clonedDiv");
+   document.getElementById('clonedBlock').appendChild(br);
+   hr.setAttribute('id','clonedHrInBlock');
+   br.setAttribute('id','clonedBrInBlock');   
+   dataNumber = parseInt(document.getElementById('clonedDiv').getAttribute('data-order'));
+   if(isNaN(dataNumber)){
+      dataNumber=1;
+      theclonedBlock.setAttribute('data-order',dataNumber);
+      return dataNumber;
+   }
+      dataNumber++;
+      theclonedBlock.setAttribute('data-order',dataNumber);
+      return dataNumber;
  }
 
- function copySectionInBlock(id){
+ function copySectionInBlock(){
    let br = document.createElement('br');
    document.getElementById('clonedDiv').appendChild(br);
    let sourceDiv = document.getElementById('sourceDiv');
@@ -59,24 +86,36 @@ function copyBlock(id){
    br.setAttribute("id","copiedBrInBlock");
 }
 
- function delFunction(id){
+ function delFunction(){
    let aboutDelete = document.getElementById('cloned');
    let deleteSpace = document.getElementById('clonedbr');   
    deleteSpace.remove();
    aboutDelete.remove();
  }
 
- function delFunctionInBlock(id){
+ function delFunctionInBlock(){
    let aboutDelete = document.getElementById('clonedInBlock');
    let deleteSpace = document.getElementById('copiedBrInBlock');   
    deleteSpace.remove();
    aboutDelete.remove();
  }
 
-function deleteDiv(id){
+function deleteDiv(){
    let divId = document.getElementById('clonedDiv');
    let hrId = document.getElementById('clonedHrInBlock');
    let brId = document.getElementById('clonedBrInBlock');
+
+   divId.remove();
+   brId.remove();
+   hrId.remove();
+
+}
+
+function deleteIndividualDiv(){
+   let divId = document.getElementById('clonedDiv');
+   let hrId = document.getElementById('clonedHrInBlock');
+   let brId = document.getElementById('clonedBrInBlock');
+
    divId.remove();
    brId.remove();
    hrId.remove();
@@ -117,7 +156,7 @@ on ('#clones', 'click','#decButton', function(e){
 
 on ('#clonedBlock', 'click','#closeBlocks', function(e){
 
-   deleteDiv();
+   deleteIndividualDiv();
 });
 
 on ('#clonedBlock', 'click','#incButton', function(e){
